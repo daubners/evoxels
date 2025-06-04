17d9ab9e0911f400b0531985e29eb9c1333021ae
daubners <s.daubner@imperial.ac.uk>
2025-06-04 19:06:45 +0100

    from monolithic solvers to modular

--- voxelsss/__init__.py
+++ voxelsss/__init__.py
@@ -1,8 +1,10 @@
 "Init all functions"
 
-from .fields import VoxelFields
-from .solvers_phasefield import PeriodicCahnHilliardSolver, MixedCahnHilliardSolver
+from .voxelfields import VoxelFields
+__all__ = ['VoxelFields']
 
-__all__ = ['VoxelFields', \
-           'PeriodicCahnHilliardSolver', \
-           'MixedCahnHilliardSolver']
+from .precompiled_solvers import run_cahn_hilliard_solver
+from .precompiled_solvers import PeriodicCahnHilliardSolver, MixedCahnHilliardSolver
+__all__.extend(['run_cahn_hilliard_solver', \
+                'PeriodicCahnHilliardSolver', \
+                'MixedCahnHilliardSolver'])
