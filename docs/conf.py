8a2e571abf7b7932801f08906cd85b21f75f352e
daubners <s.daubner@imperial.ac.uk>
2025-06-12 10:48:53 +0100

    fix autogenerated docs

--- docs/conf.py
+++ docs/conf.py
@@ -7,16 +7,35 @@ author = 'Simon Daubner'
 
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'myst_parser',
     'nbsphinx',
 ]
 
+myst_enable_extensions = [
+    'amsmath',
+    'dollarmath',
+]
+
 nb_execution_mode = 'off'
 html_theme = 'sphinx_rtd_theme'
-autodoc_mock_imports = ['matplotlib', 'pyvista', 'psutil', 'ipython', 'numpy', 'torch']
+# autodoc_mock_imports = ['matplotlib', 'pyvista', 'psutil', 'ipython', 'numpy', 'torch']
 exclude_patterns = ['_build']
+
+# Copy images referenced in the README so they appear on ReadTheDocs.
+html_extra_path = ['../voxelsss.png', '../voxelsss-graphical.png']
+
+# Automatically document all members using Google style docstrings.
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'special-members': '__init__',
+    'inherited-members': False,
+}
+# Exclude submodule voxelfields as it already is top-level
+exclude_patterns += ['**/voxelfields.rst', '**/voxelsss.voxelfields.*']
 
 # Napoleon settings
 napoleon_google_docstring = True
--- voxelsss/__init__.py
+++ voxelsss/__init__.py
@@ -1,15 +1,11 @@
 """Public API for the voxelsss package."""
 
 from .voxelfields import VoxelFields
-from .precompiled_solvers import (
-    MixedCahnHilliardSolver,
-    PeriodicCahnHilliardSolver,
+from .precompiled_solvers.cahn_hilliard import (
     run_cahn_hilliard_solver,
 )
 
 __all__ = [
     "VoxelFields",
     "run_cahn_hilliard_solver",
-    "PeriodicCahnHilliardSolver",
-    "MixedCahnHilliardSolver",
 ]
